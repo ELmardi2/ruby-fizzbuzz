d521628e17439db08d0d63f0566126a1270c6a66
ELmardi2 <mardi.yahia@gmail.com>
2016-06-02 15:39:29 +0200

    aw test

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,24 +1,51 @@
-class Integer
-   def fizzbuzz
-     class Integer
-  def fizzbuzz         class Integer
-         def fizzbuzz
-       (2 .. 30).each do |i|
-     
-          if i % 5 == 0 and i % 3 == 0
-             puts "FizzBuzz"
-          elsif i % 5 == 0
-             puts "Buzz"
-          elsif i % 3 == 0
-             puts "Fizz"
-          else
-             puts i
-       end
+def fizz_buzz_1(max)
+  arr = []
+  (1..max).each do |n|
+    if ((n % 3 == 0) && (n % 5 == 0))
+      arr << "FizzBuzz"
+    elsif (n % 3 == 0)
+      arr << "Fizz"
+    elsif (n % 5 == 0)
+      arr << "Buzz"
+    else
+      arr << n
     end
-   end
-endendputs 2.fizzbuzz
-puts 5.fizzbuzz
-puts 6.fizzbuzz
-puts 11.fizzbuzz
-puts 30.fizzbuzzend
+  end
+  return arr
+end
+
+def fizz_buzz_2(max)
+  arr = []
+  (1..max).each do |n|
+    if (n % 3 == 0)
+      if (n % 5 == 0)
+        arr << "FizzBuzz"
+      else
+        arr << "Fizz"
+      end
+    elsif (n % 5 == 0)
+      arr << "Buzz"
+    else
+      arr << n
+    end
+  end
+  return arr
+end
+
+def fizz_buzz_3(max)
+  arr = []
+  (1..max).each do |n|
+    text = ""
+    if (n % 3 == 0)
+      text << "Fizz"
+    end
+    if (n % 5 == 0)
+      text << "Buzz"
+    end
+    if !((n % 3 == 0) || (n % 5 == 0))
+      text = n
+    end
+    arr << text
+  end
+  return arr
 end
